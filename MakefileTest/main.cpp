3b69593003b4012de3c1caa063e9849dc64537c5
andrew-noonan <andrew.p.noonan@vanderbilt.edu>
2022-07-30 13:59:39 -0400

    Good open, config, and save of termios

--- MakefileTest/main.cpp
+++ MakefileTest/main.cpp
@@ -25,9 +25,15 @@ int main() {
 // -------------------------
 
   // check options by using 'ls -l /dev/t*' in terminal
-  int serial_port = open("/dev/ttyAMA0",9600);  //Serial ports are stored as files by pi
+  int serial_port = open("/dev/ttyAMA0",O_RDWR);  //Serial ports are stored as files by pi, second argument is "open for read and write"
   if (serial_port < 0){
-    printf("Error %i from open: %s", errno, strerror(errno));
+    printf("Error %ivadl@raspberrypi:~/SummerProject/MakefileTest $ ./a.out
+Started Main
+Error 20 from open: Not a directory
+Error 9 from tcgetattr: Bad file descriptor
+Error 9 from tcsetattr: &s
+Ended Main
+ from open: %s\n", errno, strerror(errno));
   }
   
   //Create new termios struct
@@ -36,7 +42,7 @@ int main() {
   // tcgetattr test -> can read termios documentation for more info
   // returns 0 if successful, sets errno and returns -1 if unsuccessful
   if(tcgetattr(serial_port, &my_tty) != 0){
-      printf("Error %i from tcgetattr: %s", errno, strerror(errno));
+      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
   }
   
   
@@ -44,35 +50,54 @@ int main() {
 // Control Modes
 // -------------------------
     
-    // Parity
+    // Parity - Useful Explanation: electronics.stackexchange.com/questions/9108/for-uart-should-i-use-parity-on-a-board-level
+    my_tty.c_cflag &= ~PARENB; //Disable parity bit
+    //my_tty.c_cflag |= PARENB; // Enable parity bit
+    
     
     // Num of Stop Bits
+    my_tty.c_cflag &= ~CSTOPB; // Clear stop bits, 1 Stop bit default
+    //my_tty.c_cflag |= CSTOPB; // 2 Stop Bits (less common)
     
     // Bits per byte
+    my_tty.c_cflag &= ~CSIZE; // Clear the size bits
+    my_tty.c_cflag |= CS8; // Set size bit to 8 bits per byte (can choose 5-8)
+    
     
     // Flow Control
     // Handshake system where data is only sent from sensor if we say we're ready
     // implemented using the RTS / CTS pins, which we have disconnected
+    my_tty.c_cflag &= ~CRTSCTS; //Disable control
+    //my_tty.c_cflag |= CRTSCTS //Enable control
+    
     
     // Cread and Clocal
+    my_tty.c_cflag |= CREAD | CLOCAL; //CREAD allows us to read 
     
 // -------------------------
 // Local Modes
 // -------------------------
 
-  // Canonical Mode
+  // Canonical Mode (process input when newline char is received)
+  my_tty.c_lflag &= ~ICANON; // Disable canonical
   
-  // Echo
+  // Echo - disable all
+  my_tty.c_lflag &= ~ECHO;
+  my_tty.c_lflag &= ~ECHOE;
+  my_tty.c_lflag &= ~ECHONL;
   
   // Signal Chars
+  my_tty.c_lflag &= ~ISIG; //Signal chars not used with serial
 
 // -------------------------
 // Input Modes
 // -------------------------
 
   // Software Flow Control
+  my_tty.c_lflag &= ~(IXON | IXOFF | IXANY);
   
-  // Special handling of bytes
+  // Special handling of bytes - Disabling all so we just get raw data
+  my_tty.c_lflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
 
 
 // -------------------------
@@ -80,22 +105,34 @@ int main() {
 // -------------------------
 
   // Prevent unwanted conversion of special chars (newline, carriage, tabs)
+  my_tty.c_oflag &= ~OPOST;
+  my_tty.c_oflag &= ~ONLCR;
   
 // -------------------------
 // Vtime && VMin
 // -------------------------
 
   // VMin and VTime are codependent - look at documentation for selection
+  // Basically determines when we stop waiting for bits after calling read()
+  my_tty.c_cc[VTIME] = 10; //Wait for up to 10 deciseconds, returning as soon as any data is received
+  my_tty.c_cc[VMIN] = 0;  //Note: IMU Measurement Register is 44 bytes
 
 // -------------------------
 // Baud Rate
 // -------------------------
 
+  // VN UART supports 9600 - 921600
+  cfsetispeed(&my_tty, B115200);
+  cfsetospeed(&my_tty, B115200);
   
 
 // -------------------------
 // Save Termios Configuration
 // -------------------------
+
+  if (tcsetattr(serial_port, TCSANOW, &my_tty) != 0){
+    printf("Error %i from tcsetattr: &s\n",errno, strerror(errno));
+  }
 
 // -------------------------
 // Read and Write Configuration
@@ -108,3 +145,4 @@ int main() {
   std::cout << "Ended Main" << std::endl;
   return 0;
 }
+
